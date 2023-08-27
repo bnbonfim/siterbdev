5ec0cd2d99270a6bd99c1ed1fefd4e97c092e122
bnbonfim <bruamorimbonfim@gmail.com>
2023-08-27 17:11:00 -0300

    fix:alterações forms

---
+++ script.js
@@ -0,0 +1,10 @@
+function abrirForm(){
+    const form = document.getElementById('janela-modal')
+    form.classList.add('abrir')
+    
+    form.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal'){
+            form.classList.remove('abrir')
+        }
+    })
+}
