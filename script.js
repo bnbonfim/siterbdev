46c7f59f2eb36bc28a56e4dba704e70018238343
bnbonfim <bruamorimbonfim@gmail.com>
2023-08-30 11:53:42 -0300

    fix: alterações

--- script.js
+++ script.js
@@ -9,5 +9,22 @@ function abrirForm(){
     })
 }
 function entrar(){
-    
+    var login = document.getElementById('name').value;
+    var senha = document.getElementById('password').value;
+
+    if(login == 'bn' && senha =="123"){
+        alert('Bem vindo(a) clique em ok.');
+        location.href = "index.html";
+    }else{
+        alert('Nome ou senha incorretos, não tem uma conta? Clique em registrar-se')
+    }
+}
+function registrar(){
+    var novoLogin = document.getElementById('name').value;
+    var novaSenha = document.getElementById('password').value;
+
+    if (novoLogin && novaSenha) {
+        alert('Conta registrada com sucesso! Agora você pode fazer login.');
+        location.href = "index.html";
+    } 
 }
